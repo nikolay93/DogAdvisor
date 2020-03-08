4092e490eb4a3a8e4cc49f570a80e92e93dcb76e
Nikolay Kolev <Nikolay.Kolev@de.rhenus.com>
2020-03-08 18:51:51 +0200

    add OfferCategoris Seeder

--- Data/DogAdvisor.Data/Seeding/ApplicationDbContextSeeder.cs
+++ Data/DogAdvisor.Data/Seeding/ApplicationDbContextSeeder.cs
@@ -27,6 +27,7 @@
                           {
                               new RolesSeeder(),
                               new SettingsSeeder(),
+                              new OfferCategoriesSeeder(),
                           };
 
             foreach (var seeder in seeders)
---
+++ Data/DogAdvisor.Data/Seeding/OfferCategoriesSeeder.cs
@@ -0,0 +1,40 @@
+﻿using DogAdvisor.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogAdvisor.Data.Seeding
+{
+    public class OfferCategoriesSeeder : ISeeder
+    {
+        public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            if (dbContext.OfferCategories.Any())
+            {
+                return;
+            }
+
+            var categories = new List<string> 
+            {
+                "Accommodation",
+                "Family attractions",
+                "Good for walks",
+                "Indoor venues",
+                "Pubs",
+                "Shops and Services",
+            };
+
+            foreach (var category in categories)
+            {
+                var offerCategory = new OfferCategory
+                {
+                    Name = category,
+                };
+
+                dbContext.OfferCategories.Add(offerCategory);
+            }
+        }
+    }
+}
